db27f867b2b317497e3a93711c209123c1322ce8
DuyCongNguyen <nguyenduycongndc@gmail.com>
2020-06-17 16:42:18 +0700

    login new

---
+++ src/app/_models/user.ts
@@ -0,0 +1,7 @@
+﻿export class User {
+    email: string;
+    access_token: string;
+    token_type?: string;
+    expires_at: string;
+    password?: string;
+}
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  name: '(TEST)',
+  apiUrl: 'http://localhost:8000/api/auth'
+};
